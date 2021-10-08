58620a992073d90881c03acac5f2b368c8b184a6
KangSuzy <astnwl321@gmail.com>
2021-10-08 19:02:41 +0900

    강수지: 백준 카드2

--- week2/suzy/2164.py
+++ week2/suzy/2164.py
@@ -3,6 +3,13 @@ from collections import deque
 
 n = int(input())
 d = deque()
-for i in range(1,n+1):
-    d.append(i)
-print(list(d))
+# 1부터 n까지 덱
+for i in range(n):
+    d.append(i+1)
+
+while len(d)>1:
+    # 맨 위 카드를 꺼내고 아래로 옮기기
+    d.popleft()
+    d.rotate(-1)
+print(d.pop())
+
