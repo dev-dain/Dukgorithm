255bf0013ef5989ed9a5838f5d6ff5a7958cc5f0
KangSuzy <astnwl321@gmail.com>
2021-10-13 17:19:44 +0900

    강수지: 백준 11866 코드 수정(deque rotate 사용)

--- week2/suzy/11866.py
+++ week2/suzy/11866.py
@@ -2,24 +2,17 @@ from collections import deque
 
 # 요세푸스 순열 (n 사람수, k 순서 제거)
 n, k = map(int, input().split())
-yo = deque()
+# range 사용
+yo = deque(range(1, n+1))
 ans = []
-
-for i in range(n):
-    yo.append(i+1)
 
 # k 번째 원소를 제거하기 위해
 temp = k-1
 
 while yo:
-    for i in range(k):
-        if i == temp:
-            p = yo.popleft()
-            ans.append(p)
-        else:
-            p = yo.popleft()
-            yo.append(p)
-
+    # rotate 사용
+    yo.rotate(-k)
+    ans.append(str(yo.pop()))
 print('<'+', '.join(map(str, ans))+'>')
 
 
