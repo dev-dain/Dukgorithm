e736b3ed33dc918bacfb5dd8d6326951a1c3b7d8
KangSuzy <astnwl321@gmail.com>
2021-10-25 00:35:06 +0900

    강수지: 프로그래머스 42626

---
+++ week2/suzy/42626.py
@@ -0,0 +1,13 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        mix = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, mix)
+        answer += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    return answer
