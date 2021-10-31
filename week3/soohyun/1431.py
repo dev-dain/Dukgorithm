3c2c37d2954d659c6b25ecf75380e8a3399893f6
soohyun <soohyun6879@naver.com>
2021-10-31 18:44:48 +0900

    장수현: 백준 시리얼번호 문제풀이

--- week3/soohyun/1431.py
+++ week3/soohyun/1431.py
@@ -7,7 +7,7 @@ def s_num(x):
 	result = [int(n) for n in x if n.isdigit()]
 	return sum(result)
 
-serial.sort(key=lambda x : (len(x), s_num(x), x)) # 갈이 -> 숫자 합 -> 사전 순으로 정렬
+serial.sort(key=lambda x : (len(x), s_num(x), x)) # 길이 -> 숫자 합 -> 사전 순으로 정렬
 
 for s in serial:
 	print(s)
