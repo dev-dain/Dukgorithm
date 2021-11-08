d06b7e8802c924a3cbeb8f71cf0ba8d724d4e1dd
KangSuzy <astnwl321@gmail.com>
2021-11-08 14:22:25 +0900

    강수지: 백준 2193

---
+++ week4/suzy/2193.py
@@ -0,0 +1,18 @@
+import sys
+n = int(sys.stdin.readline())
+cnt = 0
+
+# dp
+dp = [1 for i in range(91)] # 0부터 90의 범위
+dp[1] = 1 # 1
+dp[2] = 1 # 10
+# dp[3] = 2 # 100 101
+# dp[4] = 3 # 1000 1001 1010
+
+for i in range(1, n+1):
+    if i == 1 or i == 2:
+        dp[i] = 1
+    else:
+        dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[i])
